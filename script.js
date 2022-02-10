dc0a141047c5c4d4463befc015d250b10f4f62bf
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-10 20:20:19 +0530

    fix - id selectors replaced by class

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const scrollTopBtn = document.querySelector('#scroll-top-btn')
+const scrollTopBtn = document.querySelector('.scroll-top-btn')
 
 scrollTopBtn.addEventListener('click', () => {
-    // console.log('cc')
     window.scrollTo(0,0)
 })
