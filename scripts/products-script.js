07d4162191af73c06976e8e30e34e012ededc1bf
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-24 01:55:12 +0530

    style - filter component ui updated

--- scripts/products-script.js
+++ scripts/products-script.js
@@ -1,5 +1,19 @@
-const scrollTopBtn = document.querySelector('.scroll-top-btn')
+const toggleFiltersBtn = document.querySelector('#btn-filters')
+const asideFilters = document.querySelector('.aside-filters')
+const mainProducts = document.querySelector('#main-prdlist')
+const priceFilter = document.querySelector('#filter-price')
+const priceFilterVal = document.querySelector('#filter-priceval')
 
-scrollTopBtn.addEventListener('click', () => {
-    window.scrollTo(0,0)
+toggleFiltersBtn.addEventListener('click', () => {
+    asideFilters.classList.remove('dis-hidden')
+    toggleFiltersBtn.classList.add('dis-hidden')
+})
+
+mainProducts.addEventListener('click', () => {
+    asideFilters.classList.add('dis-hidden')
+    toggleFiltersBtn.classList.remove('dis-hidden')
+})
+
+priceFilter.addEventListener('input', (e) => {
+    priceFilterVal.innerText = e.target.value
 })
