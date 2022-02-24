411d3ae3503ad93f579e47cd703da48becf2c385
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-24 15:33:56 +0530

    style - product list page ui updated

--- scripts/products-script.js
+++ scripts/products-script.js
@@ -3,6 +3,7 @@ const asideFilters = document.querySelector('.aside-filters')
 const mainProducts = document.querySelector('#main-prdlist')
 const priceFilter = document.querySelector('#filter-price')
 const priceFilterVal = document.querySelector('#filter-priceval')
+const starIcons = document.querySelectorAll('#star-icon')
 
 toggleFiltersBtn.addEventListener('click', () => {
     asideFilters.classList.remove('dis-hidden')
@@ -16,4 +17,18 @@ mainProducts.addEventListener('click', () => {
 
 priceFilter.addEventListener('input', (e) => {
     priceFilterVal.innerText = e.target.value
+})
+
+starIcons.forEach(starIcon => {
+    starIcon.addEventListener('click', (e) => {
+        const maxRating = e.target.dataset.value
+        starIcons.forEach(star => {
+            if(star.dataset.value <= maxRating) {
+                star.classList.add('txt-warn')  
+            } else {
+                star.classList.remove('txt-warn')
+                star.classList.add('txt-off-secondary')  
+            }
+        })
+    })
 })
