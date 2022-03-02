d47a44a81d00fe32379562c0bc32e9e48d403a5c
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-02 14:33:06 +0530

    style - login page ui update

--- scripts/products-script.js
+++ scripts/products-script.js
@@ -5,20 +5,24 @@ const priceFilter = document.querySelector('#filter-price')
 const priceFilterVal = document.querySelector('#filter-priceval')
 const starIcons = document.querySelectorAll('#star-icon')
 
+// show filters menu
 toggleFiltersBtn.addEventListener('click', () => {
     asideFilters.classList.remove('dis-hidden')
     toggleFiltersBtn.classList.add('dis-hidden')
 })
 
+// hide filters menu
 mainProducts.addEventListener('click', () => {
     asideFilters.classList.add('dis-hidden')
     toggleFiltersBtn.classList.remove('dis-hidden')
 })
 
+// price slider input value change
 priceFilter.addEventListener('input', (e) => {
     priceFilterVal.innerText = e.target.value
 })
 
+// rating bar functionality
 starIcons.forEach(starIcon => {
     starIcon.addEventListener('click', (e) => {
         const maxRating = e.target.dataset.value
